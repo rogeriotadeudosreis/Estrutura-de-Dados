d7c27b86deb1eb170995ff14881e8d05ee178390
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr46@gmail.com>
2020-09-28 16:25:35 -0300

    Modificado

--- src/parte_1_exercicios_metodosdeordenacao_2020_2/Exercicio_1_MetodoDeOrdenacao_SelecaoEbolha.java
+++ src/parte_1_exercicios_metodosdeordenacao_2020_2/Exercicio_1_MetodoDeOrdenacao_SelecaoEbolha.java
@@ -1,14 +1,23 @@
+/*
+Método de ordenação por seleção (SelectionSort).
+Algoritmo simples, onde ele procura o menor elemento
+e coloca na primeira posição do vetor.
+Não recomendado para grande conjunto de dados
+link > https://www.youtube.com/watch?v=ilv2U7jRArQ
+*/
+
+
 package parte_1_exercicios_metodosdeordenacao_2020_2;
 
 public class Exercicio_1_MetodoDeOrdenacao_SelecaoEbolha {
 
     public static void main(String[] args) {
 
-        int[] numA = {1, -13, 5, 8, 10, 12};
+        int[] numA = {32, -13, 5, 8, 10, 12, 6, 3, 1};
         
 
         for (int i = 0; i < numA.length; i++) {
-            int menor = 1;
+            int menor = i;
             for (int j = i + 1; j < numA.length; j++) {
                 if (numA[menor] > numA[j]) {
                     menor = j;
@@ -19,6 +28,7 @@ public class Exercicio_1_MetodoDeOrdenacao_SelecaoEbolha {
             numA[menor] = aux;
             System.out.print(numA[i] + " ");
         }
+        System.out.println("");
 
     }
 
