c85119cc3a75535c680074e6848eef146b17837a
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr46@gmail.com>
2020-10-27 15:17:45 -0300

    Commit método String CharAt adicionado

--- src/exercicios_casos_de_estudos/Metodo_string_CharAt.java
+++ src/exercicios_casos_de_estudos/Metodo_string_CharAt.java
@@ -9,8 +9,13 @@ public class Metodo_string_CharAt {
         char[] carac = new char[256];
         
         for (int i = 0; i < valor.length(); i++) {
+            System.out.printf("%c ", valor.charAt(i));
+        }
+        
+        for (int i = 0; i < valor.length(); i++) {
             carac[i] = valor.charAt(i);
         }
+        System.out.println("");
         for (int i = 0; i < carac.length; i++) {
             if (carac[i] > 0) {
             System.out.print(carac[i] + " ");
